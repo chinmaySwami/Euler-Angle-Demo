5d867a5d77ad7c0404f14561daecf9071413e632
Chinmay Swami <chinmayasami@gmail.com>
2020-08-16 13:35:56 -0400

    updated euler test

--- Euler_Test.m
+++ Euler_Test.m
@@ -36,50 +36,58 @@ R_X = [1 0 0; 0 cosd(alp) -sind(alp); 0 sind(alp) cosd(alp)];
 R_Y = [cosd(bet) 0 sind(bet) ; 0 1 0; -sind(bet) 0 cosd(bet)];
 R_Z = [cosd(gam) -sind(gam) 0; sind(gam) cosd(gam) 0; 0 0 1];
 
-% R_Mat = R_Z*R_Y*R_X
-for i = 1:3
-    if i == 1
-        z = R_Z * XYZ;
+% Plotting a vector perpendicular to a plane formed by 2 other vectors
+% vect1 = [XYZ_Origin;3 5 3];
+% plot3(vect1(:,1),vect1(:,2),vect1(:,3),'-.c', 'LineWidth',0.5);
+% 
+% vectx1 = cross(vect1, vecx);
+% plot3(vectx1(:,1),vectx1(:,2),vectx1(:,3),'y', 'LineWidth',0.5);
 
-        vecx = [XYZ_Origin; z(1,:)];
-        vecy = [XYZ_Origin; z(2,:)];
-        vecz = [XYZ_Origin; z(3,:)];
 
-        plot3(vecx(:,1),vecx(:,2),vecx(:,3),'-.r', 'LineWidth',1);
-        plot3(vecy(:,1),vecy(:,2),vecy(:,3),'-.g', 'LineWidth',1);
-        plot3(vecz(:,1),vecz(:,2),vecz(:,3),'-.b', 'LineWidth',1);
-        disp('Rotating around x Axis')
-        title('Rotated around Z axis by 90 degrees')
-        disp(z)
-        pause(8)
-    end
-    if i == 2
-        z = R_X * z;
-
-        vecx = [XYZ_Origin; z(1,:)];
-        vecy = [XYZ_Origin; z(2,:)];
-        vecz = [XYZ_Origin; z(3,:)];
-
-        plot3(vecx(:,1),vecx(:,2),vecx(:,3),'--r', 'LineWidth',1);
-        plot3(vecy(:,1),vecy(:,2),vecy(:,3),'--g', 'LineWidth',1);
-        plot3(vecz(:,1),vecz(:,2),vecz(:,3),'--b', 'LineWidth',1);
-        disp('Rotating around Y Axis')
-        title('Rotated around X axis by 90 degrees')
-        disp(z)
-        pause(8)
-    end
-    if i == 3
-        z = R_Y * z;
-
-        vecx = [XYZ_Origin; z(1,:)];
-        vecy = [XYZ_Origin; z(2,:)];
-        vecz = [XYZ_Origin; z(3,:)];
-
-        plot3(vecx(:,1),vecx(:,2),vecx(:,3),'--r', 'LineWidth',2);
-        plot3(vecy(:,1),vecy(:,2),vecy(:,3),'--g', 'LineWidth',2);
-        plot3(vecz(:,1),vecz(:,2),vecz(:,3),'--b', 'LineWidth',2);
-        title('Rotated around Y axis by 90 degrees')
-        disp(z)
-    end
-   
-end
+% % R_Mat = R_Z*R_Y*R_X
+% for i = 1:3
+%     if i == 1
+%         z = R_Z * XYZ;
+% 
+%         vecx = [XYZ_Origin; z(1,:)];
+%         vecy = [XYZ_Origin; z(2,:)];
+%         vecz = [XYZ_Origin; z(3,:)];
+% 
+%         plot3(vecx(:,1),vecx(:,2),vecx(:,3),'-.r', 'LineWidth',1);
+%         plot3(vecy(:,1),vecy(:,2),vecy(:,3),'-.g', 'LineWidth',1);
+%         plot3(vecz(:,1),vecz(:,2),vecz(:,3),'-.b', 'LineWidth',1);
+%         disp('Rotating around x Axis')
+%         title('Rotated around Z axis by 90 degrees')
+%         disp(z)
+%         pause(8)
+%     end
+%     if i == 2
+%         z = R_X * z;
+% 
+%         vecx = [XYZ_Origin; z(1,:)];
+%         vecy = [XYZ_Origin; z(2,:)];
+%         vecz = [XYZ_Origin; z(3,:)];
+% 
+%         plot3(vecx(:,1),vecx(:,2),vecx(:,3),'--r', 'LineWidth',1);
+%         plot3(vecy(:,1),vecy(:,2),vecy(:,3),'--g', 'LineWidth',1);
+%         plot3(vecz(:,1),vecz(:,2),vecz(:,3),'--b', 'LineWidth',1);
+%         disp('Rotating around Y Axis')
+%         title('Rotated around X axis by 90 degrees')
+%         disp(z)
+%         pause(8)
+%     end
+%     if i == 3
+%         z = R_Y * z;
+% 
+%         vecx = [XYZ_Origin; z(1,:)];
+%         vecy = [XYZ_Origin; z(2,:)];
+%         vecz = [XYZ_Origin; z(3,:)];
+% 
+%         plot3(vecx(:,1),vecx(:,2),vecx(:,3),'--r', 'LineWidth',2);
+%         plot3(vecy(:,1),vecy(:,2),vecy(:,3),'--g', 'LineWidth',2);
+%         plot3(vecz(:,1),vecz(:,2),vecz(:,3),'--b', 'LineWidth',2);
+%         title('Rotated around Y axis by 90 degrees')
+%         disp(z)
+%     end
+%    
+% end
